c25709cc52d21001eb284ecdb4a5052b538321b6
Ryan S <snowr1024@gmail.com>
2025-09-14 13:57:24 -0500

    added nn search and DBSCAN logic to explo

--- analysis/main.py
+++ analysis/main.py
@@ -9,6 +9,8 @@ import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import pandas as pd
 
+from sklearn.neighbors import NearestNeighbors
+
 this_dir = pathlib.Path('.').parent.resolve()
 data_dir = this_dir / 'data'
 
@@ -29,14 +31,29 @@ if __name__ == "__main__":
     all_data = []
     for workout in workouts:
         print(f"Processing {workout.name}")
+        if 'Cycling' not in workout.name:
+            continue
         df = process_fit(workout)
         df = df.with_columns(pl.lit(workout.name).alias('workout_name'))
         all_data.append(df.to_pandas())
 
-    combined_df = pd.concat(all_data, ignore_index=True)    
+    combined_df = pd.concat(all_data, ignore_index=True)
+    combined_df = pl.from_pandas(combined_df)
+    combined_df = combined_df.drop_nulls(subset=['position_lat', 'position_long', 'altitude'])
+
     print(combined_df)
 
-    fig = px.scatter_3d(combined_df, x='position_lat', y='position_long', z='altitude',)
-    fig.update_layout(
-    scene=dict(aspectmode='manual', aspectratio=dict(x=1, y=1, z=0.1)))
-    fig.show(renderer="browser")
+    distances = NearestNeighbors(n_neighbors=25).fit(combined_df.select(['position_long', 'position_lat']).to_numpy())
+    distances, _ = distances.kneighbors(combined_df.select(['position_long', 'position_lat']).to_numpy())
+    print(distances.shape)
+    combined_df.write_parquet(data_dir / 'combined.parquet')
+
+
+
+    # fig = px.box(x=distances)
+    # fig.show(renderer="browser")
+
+    # fig = px.scatter(combined_df, x='position_long', y='position_lat', color='altitude',)
+    # fig.update_layout(
+    # scene=dict(aspectmode='manual', aspectratio=dict(x=1, y=1, z=0.1)))
+    # fig.show(renderer="browser")
